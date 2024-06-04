d439310d32471548e5525fe0cc80eb740c167a9c
npj12 <juniornango@gmail.com>
2024-06-04 00:34:31 +0000

    endpoints for /authenticate route

---
+++ backend/api/routes/authenticate.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res, next)=>{
+    res.status(200).json({
+        message: "Fecthing all authenticates"
+    });
+});
+
+router.post("/",  (req, res, next)=>{
+    res.status(201).json({
+        message: "New authenticade made"
+    });
+});
+
+router.get("/:id", (req, res, next) =>{
+    const id = req.params.id;
+    res.status(200).json({
+        message: `Fetching authenticate. Id: '${id}'`
+    });
+});
+
+router.use((req, res, next)=>{
+    res.status(404).json({
+        error: "Not Found"
+    });
+});
+
+module.exports = router;
--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
+const authenticateRouter = require("./api/routes/authenticate");
 
-
+app.use("/authenticate", authenticateRouter)
 
 module.exports = app;
