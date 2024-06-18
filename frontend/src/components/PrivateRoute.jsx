13938f22a4bc72747f238bce97722da32d15a489
hans <hans.ngainsu@facsciences-uy1.cm>
2024-06-18 15:12:42 +0100

    second commit du fichier authcontext.jsx et privateRoute.jsx

---
+++ frontend/src/components/PrivateRoute.jsx
@@ -0,0 +1,12 @@
+// src/components/PrivateRoute.jsx
+import React from 'react';
+import { Navigate, Outlet } from 'react-router-dom';
+import { useAuth } from '../context/AuthContext';
+
+const PrivateRoute = () => {
+  const { isAuthenticated } = useAuth();
+
+  return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
+};
+
+export default PrivateRoute;
