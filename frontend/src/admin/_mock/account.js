1bc9f8c68afd022026f7ce413b763e55b11fdd89
adams <kaldadak@facsciences-uy1.cm>
2024-06-18 20:32:36 +0100

    Add admin pages

---
+++ frontend/src/admin/_mock/account.js
@@ -0,0 +1,7 @@
+// ----------------------------------------------------------------------
+
+export const account = {
+  displayName: 'K. ADAMS',
+  email: 'adams@betterave.cm',
+  photoURL: '/assets/images/avatars/avatar_25.jpg',
+};
