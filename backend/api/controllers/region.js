d57498c6b4b641d9f42959696732d19bf1e78d0f
npj12 <juniornango@gmail.com>
2024-06-20 16:26:17 +0000

    Update Fetch the region List from DB

--- backend/api/controllers/region.js
+++ backend/api/controllers/region.js
@@ -1,16 +1,31 @@
+const { Client } = require('pg');
+const { dbConfig } = require("../constants");
+
+
 exports.getAllRegions = (req, res, next)=>{
-    const jsonResponse = [
-        'Adamaoua', 
-        'Center', 
-        'East', 
-        'Extreme nord', 
-        'Littoral', 
-        'Ngaoundere', 
-        'Nord', 
-        'Nord-West', 
-        'West', 
-        'South-West'
-    ]
-    console.log(jsonResponse);
-    return res.status(200).json(jsonResponse);
+    const client = new Client(dbConfig);
+    client.connect()
+        .then(()=>{
+            const query = 'SELECT * FROM region';
+            client.query(query)
+                .then(result => {
+                    const jsonResponse = result.rows;
+                    console.log(jsonResponse);
+                    return res.status(200).json(jsonResponse);
+                })
+                .catch(error=>{ 
+                    const jsonResponse = {
+                        error: error
+                    };
+                    console.log(jsonResponse);
+                    res.status(500).json(jsonResponse);
+                });
+        })
+        .catch(error=>{ 
+            const jsonResponse = {
+                error: error
+            };
+            console.log(jsonResponse);
+            res.status(500).json(jsonResponse);
+        });
 }
