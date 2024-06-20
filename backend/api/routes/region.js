340769c92b198807543535d4cc4d42fe1ae67b10
npj12 <juniornango@gmail.com>
2024-06-20 13:52:46 +0000

    Add documentation of the route

--- backend/api/routes/region.js
+++ backend/api/routes/region.js
@@ -2,6 +2,17 @@ const express = require("express");
 const router = express.Router();
 const regionController = require('../controllers/region');
 
+/**
+ * @openapi
+ * '/api/region':
+ *  get:
+ *     tags:
+ *     - Region
+ *     summary: Get the list of regions
+ *     responses:
+ *      200:
+ *        description: Fetched Successfully  
+ */
 router.get('/', regionController.getAllRegions);
 
 
